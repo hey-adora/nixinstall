af5a893a1f163a89452483953e129e54964388ad
hey <146812294+hey-adora@users.noreply.github.com>
2025-06-23 17:33:08 +0300

    generate hardware info

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 sudo mount -o remount,size=10G,noatime /nix/.rw-store
+nixos-generate-config --root . --no-filesystems
 sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' --  --flake '.#b650'
