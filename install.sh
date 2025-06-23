8cfbcba31b93fbecfae56bc6cafbdb9bbd73e7ea
hey <146812294+hey-adora@users.noreply.github.com>
2025-06-23 17:37:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,9 @@
 
 sudo mount -o remount,size=10G,noatime /nix/.rw-store
 nixos-generate-config --root . --no-filesystems
+cp ./etc/nixos/hardware-configuration.nix hardware-configuration.nix
+rm ./etc/nixos/configuration.nix 
+rm ./etc/nixos/hardware-configuration.nix
+rmdir ./etc/nixos
+rmdir ./etc
 sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' --  --flake '.#b650'
